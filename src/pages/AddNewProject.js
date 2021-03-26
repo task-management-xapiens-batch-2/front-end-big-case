e895868e83a8decd57dc58c2a438bc3699403dd6
Andang Vijayanto Utomo <andang.utomo@xapiens.id>
2021-03-26 23:14:35 +0700

    adding UI improvement in  add new project page

--- src/pages/AddNewProject.js
+++ src/pages/AddNewProject.js
@@ -1,9 +1,28 @@
 import React from "react";
+import ButtonComponent from "../components/Button.component";
+import InputComponent from "../components/Input.component";
 
 const AddNewProject = () => {
   return (
-    <div>
+    <div className="container-fluid">
       <h1>Add New Project</h1>
+      <div>
+        <InputComponent
+          type="text"
+          placeholder="Enter your project title"
+          label="Project Title"
+        />
+        <InputComponent
+          type="text"
+          placeholder="Search for a worker"
+          label="Assign a Worker"
+        />
+        <textarea />
+      </div>
+      <div>
+        <ButtonComponent title="Draft" />
+        <ButtonComponent title="Submit" />
+      </div>
     </div>
   );
 };
