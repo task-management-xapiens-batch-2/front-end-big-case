5e63dd010629eab442806f635bf55d4b56ec0385
Faris Adlin <faris.adlin@xapiens.id>
2021-03-26 19:20:58 +0700

    Used SASS

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import TaskManagement from "./pages/TaskManagement";
+import "./styles/index.scss";
+import "bootstrap/dist/css/bootstrap.css";
 
 function App() {
   return <TaskManagement />;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./styles/index.css";
+
 import App from "./App";
-import "bootstrap/dist/css/bootstrap.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
