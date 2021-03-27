57d43e6bca5dfea8a5357df60508e29c6ffe6b88
Faris Adlin <faris.adlin@xapiens.id>
2021-03-27 14:30:41 +0700

    Revert "adding UI improvement in  add new project page"
    This reverts commit e895868e83a8decd57dc58c2a438bc3699403dd6.

--- src/pages/AddNewProject.js
+++ src/pages/AddNewProject.js
@@ -1,29 +1,10 @@
 import React from "react";
-import { useHistory } from "react-router";
-import ButtonComponent from "../components/Button.component";
-import DatePickerComponent from "../components/DatePicker.component";
-import InputComponent from "../components/Input.component";
-import OptionSelector from "../components/Selector.component";
 
 const AddNewProject = () => {
   const history = useHistory();
   return (
-    <div className="add-project-section container-fluid">
-      <h1>Create New Project</h1>
-      <div>
-        <InputComponent
-          type="text"
-          placeholder="Enter your project name"
-          label="Project Name"
-        />
-        <DatePickerComponent />
-        <OptionSelector label="Assign a Worker" />
-        <textarea rows="3" cols="auto" />
-      </div>
-      <div>
-        <ButtonComponent title="Draft" onClick={() => history.push("/")} />
-        <ButtonComponent title="Submit" onClick={() => history.push("/")} />
-      </div>
+    <div>
+      <h1>Add New Project</h1>
     </div>
   );
 };
