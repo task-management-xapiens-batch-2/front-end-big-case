1b87f69953ffb5939fd3e92c7385baebbd3e1316
Faris Adlin <faris.adlin@xapiens.id>
2021-03-28 08:31:50 +0700

    Fixing the Admin Dashboard

--- src/pages/dashboard/admin/DashboardAdmin.js
+++ src/pages/dashboard/admin/DashboardAdmin.js
@@ -6,6 +6,7 @@ import { GET_USER } from "../../../graphql/queries";
 // import ButtonComponent from "../components/Button.component";
 import JumbotronComponent from "../../../components/Jumbotron.component"
 import TableComponent from "../../../components/Table.component";
+import NavbarAdminComponent from "../../../components/NavbarAdmin.component";
 
 const DashboardAdmin = () => {
   const history = useHistory();
@@ -20,19 +21,7 @@ const DashboardAdmin = () => {
       <div className="row">
         <div className="col-sm-12">
           <JumbotronComponent />
-          <div className="container-fluid">
-            <div className="row">
-              <div className="col-sm">
-                <TableComponent title="Initiatives" status="Pending" />
-              </div>
-              <div className="col-sm">
-                <TableComponent title="On Going" status="Not Started" />
-              </div>
-              <div className="col-sm">
-                <TableComponent title="Reject" status="Rejected" />
-              </div>
-            </div>
-          </div>
+          <NavbarAdminComponent />
         </div>
       </div>
     </div>
