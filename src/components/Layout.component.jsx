0fe39cc6780df2de0df6d684d567f1143e5c041e
Faris Adlin <faris.adlin@xapiens.id>
2021-04-01 14:20:29 +0700

    Added a new branch for redux-version

--- src/components/Layout.component.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const LayoutComponent = ({ children }) => {
-  return <main style={{ minHeight: "100vh" }}>{children}</main>;
-};
-
-export default LayoutComponent;
--- src/pages/TaskManagement.js
+++ src/pages/TaskManagement.js
@@ -1,59 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch, useRouteMatch } from "react-router-dom";
-import FooterBar from "../components/Footer.component";
-import LayoutComponent from "../components/Layout.component";
-import DashboardAdmin from "./dashboard/admin/DashboardAdmin";
-// import AddNewProject from "./AddNewProject";
-import Dashboard from "./dashboard/Index";
-import DashboardPlanner from "./dashboard/planner/DashboardPlanner";
-import DashboardSupervisor from "./dashboard/supervisor/DashboardSupervisor";
-import DetailProjectPlanner from "./dashboard/planner/DetailProject";
-import DetailProjectSupervisor from "./dashboard/supervisor/DetailProject";
-import AddNewProject from './dashboard/planner/AddNewProject'
-
-import Login from "./Login";
-import MainTemplateProject from "./dashboard/planner/projectForm/MainTemplateProject";
-import NavigationBar from "../components/NavbarAllUser.component";
+import Login from "./Login"
 
 const TaskManagement = () => {
   return (
     <div>
       <Router>
-        {/* <NavigationBar /> */}
-        <LayoutComponent>
           <Switch>
             <Route exact path="/" component={Login} />
-            <Route exact path="/dashboard" component={Dashboard} />
-            <Route exact path="/dashboard/admin" component={DashboardAdmin} />
-            <Route
-              exact
-              path="/dashboard/planner"
-              component={DashboardPlanner}
-            />
-            <Route
-              exact
-              path="/dashboard/supervisor"
-              component={DashboardSupervisor}
-            />
-            <Route
-              exact
-              path="/dashboard/planner/detail-project"
-              component={DetailProjectPlanner}
-            />
-            <Route
-              exact
-              path="/dashboard/supervisor/detail-project"
-              component={DetailProjectSupervisor}
-            />
-            <Route
-              exact
-              path="/dashboard/planner/new-project"
-              component={AddNewProject}
-            />
-            
           </Switch>
-        </LayoutComponent>
-        <FooterBar />
       </Router>
     </div>
   );
